7202c95f21da9f192f8dc3ff0a1c3b17ecb1ac8a
Neal Robben <neal.robben@gmail.com>
2016-06-15 22:13:55 +0200

    Added comments

--- GitHubApiWrapper/GitHubApiWrapper/GitHubWrapper.cs
+++ GitHubApiWrapper/GitHubApiWrapper/GitHubWrapper.cs
@@ -14,17 +14,32 @@ namespace GitHubApiWrapper
         private const string UserAgent = @"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
         private const string AcceptHeader = @"text/html, application/xhtml+xml, */*";
 
+        /// <summary>
+        /// Get a User based on the login of the user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         public User GetUser(string userName)
         {
             var userWrapper = new UserWrapper(AcceptHeader, UserAgent);
             return userWrapper.GetUser(userName);
         }
 
+        /// <summary>
+        /// Get the Repositories for a User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public IList<Repository> GetRepositoriesForUser(User user)
         {
             return GetRepositoriesForUser(user.Login);
         }
 
+        /// <summary>
+        /// Get the Repositories for a user login
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         public IList<Repository> GetRepositoriesForUser(string username)
         {
             var repositoryWrapper = new RepositoryWrapper(AcceptHeader, UserAgent);
